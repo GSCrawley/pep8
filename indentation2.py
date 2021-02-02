1232b1d8056d1b6ecc924bef6c22658d3c701178
GSCrawley <gscrawley@gmail.com>
2021-02-02 12:11:58 -0800

    pylint won't install for some reason; refactored by eye instead

---
+++ indentation2.py
@@ -0,0 +1,13 @@
+
+def write_to_db():
+    print('person info are written into db.')
+    if not first_name:
+        print('first name is empty.') 
+    if not last_name:
+        print('last name is empty.')
+    # ...    
+    write_to_db()
+
+photo_path = "https://en.wikipedia.org/wiki/Tim_Hunt#/media/File:Tim_Hunt_at_UCSF_05_2009_(4).jpg"
+set_person_info('Tim', 'Hunt', 'male','19 February 1943', photo_path, 
+'United Kingdom', 'Neston, Cheshire, England')
